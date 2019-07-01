af2bc4b55c614af3800e08c6731e1015206e5e4b
JJWK <35568858+JJWK@users.noreply.github.com>
2019-07-01 14:23:27 -0300

    Update at_1.sh

--- Modem/at_1.sh
+++ Modem/at_1.sh
@@ -12,6 +12,48 @@ echo -ne 'AT+UPSV?\r' > /dev/ttyUSB0
 sleep 2
 echo -ne 'AT+CCLK?\r' > /dev/ttyUSB0
 sleep 2
+####
+echo -ne 'AT\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'ATE0\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CMEE=2\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CGMI\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CGMR\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+ATI9\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CLCK="SC,2"\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CPIN?\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+UPSV?\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CCLK?\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+COPS?\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CREG=2\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CREG?\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CREG=0\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CSQ\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+UREG?\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CGMI?\r' > /dev/ttyUSB0
+sleep 2
+echo -ne 'AT+CREG?\r' > /dev/ttyUSB0
+sleep 2
+
+
+
+
+
 
 #echo -ne 'AT+CLCK="SC",2\r' >/dev/ttyUSB0
 #echo -ne 'AT+CLCK="SC",2\r' >/dev/ttyUSB0
