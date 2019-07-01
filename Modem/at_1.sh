e2888e12742fe828b2199d234f164d04b05f168a
JJWK <35568858+JJWK@users.noreply.github.com>
2019-07-01 14:11:27 -0300

    Update at_1.sh

--- Modem/at_1.sh
+++ Modem/at_1.sh
@@ -2,15 +2,15 @@
 #lxterminal --geometry=80x5 --working-directory=Documents/github/remotelab/Modem
 echo -ne 'ati\r' > /dev/ttyUSB0
 wait ${!} 
-echo -ne 'AT+CLCK="SC",2\r' >/dev/ttyUSB0
+echo -ne 'AT+CLCK="SC",2\r' > /dev/ttyUSB0
 wait ${!} 
-echo -ne 'AT+CPIN?\r' >/dev/ttyUSB0
+echo -ne 'AT+CPIN?\r' > /dev/ttyUSB0
 wait ${!} 
-echo -ne 'AT+CFUN?\r' >/dev/ttyUSB0
+echo -ne 'AT+CFUN?\r' > /dev/ttyUSB0
 wait ${!} 
-echo -ne 'AT+UPSV?\r' >/dev/ttyUSB0
+echo -ne 'AT+UPSV?\r' > /dev/ttyUSB0
 wait ${!} 
-echo -ne 'AT+CCLK?\r' >/dev/ttyUSB0
+echo -ne 'AT+CCLK?\r' > /dev/ttyUSB0
 wait ${!} 
 
 #echo -ne 'AT+CLCK="SC",2\r' >/dev/ttyUSB0
