2458f11987eac6cceaff307dbde97d3d32a79ad2
JJWK <35568858+JJWK@users.noreply.github.com>
2019-07-01 14:14:19 -0300

    Update at_1.sh

--- Modem/at_1.sh
+++ Modem/at_1.sh
@@ -1,17 +1,17 @@
 #putty -serial /dev/ttyUSB0 #Abre  puert o USB como serial 
 #lxterminal --geometry=80x5 --working-directory=Documents/github/remotelab/Modem
 echo -ne 'ati\r' > /dev/ttyUSB0
-
+sleep 2
 echo -ne 'AT+CLCK="SC",2\r' > /dev/ttyUSB0
- 
+sleep 2
 echo -ne 'AT+CPIN?\r' > /dev/ttyUSB0
-
+sleep 2
 echo -ne 'AT+CFUN?\r' > /dev/ttyUSB0
-
+sleep 2
 echo -ne 'AT+UPSV?\r' > /dev/ttyUSB0
-
+sleep 2
 echo -ne 'AT+CCLK?\r' > /dev/ttyUSB0
-
+sleep 2
 
 #echo -ne 'AT+CLCK="SC",2\r' >/dev/ttyUSB0
 #echo -ne 'AT+CLCK="SC",2\r' >/dev/ttyUSB0
